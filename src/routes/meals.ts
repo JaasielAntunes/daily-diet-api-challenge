28227f8846824c6bf7dc75d677a6786a57c6fb76
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-02-20 12:54:29 -0300

    Rota para atualizar refeição

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -62,4 +62,44 @@ export async function mealsRoutes(app: FastifyInstance) {
       return res.status(200).send({ meal });
     },
   );
+
+  app.put(
+    "/:mealId",
+    { preHandler: [checkSessionIdExists] },
+    async (req, res) => {
+      const getMealParams = z.object({
+        mealId: z.string().uuid(),
+      });
+
+      const { mealId } = getMealParams.parse(req.params);
+
+      const updateMealBody = z.object({
+        name: z.string(),
+        description: z.string(),
+        isOnDiet: z.boolean(),
+        date: z.coerce.date(),
+      });
+
+      const { name, description, isOnDiet, date } = updateMealBody.parse(
+        req.body,
+      );
+
+      const meal = await knex("meals").where({ id: mealId }).first();
+
+      if (!meal) {
+        return res.status(404).send({ error: "Refeição não encontrada!" });
+      }
+
+      await knex("meals").where({ id: mealId }).update({
+        name,
+        description,
+        is_on_diet: isOnDiet,
+        date: date.getTime(),
+      });
+
+      return res
+        .status(200)
+        .send({ message: "Refeição atualizada com sucesso!", meal });
+    },
+  );
 }
