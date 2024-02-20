4e9d280476d9a530f1d2213c7616aa440259e8e7
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-02-20 12:01:56 -0300

    Rota para buscar refeições de um usuário e buscar refeição específica

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -2,17 +2,18 @@ import { FastifyInstance } from "fastify";
 import { z } from "zod";
 import { knex } from "../database";
 import { randomUUID } from "node:crypto";
+import { checkSessionIdExists } from "../middlewares/check-session-id-exists";
 
 export async function mealsRoutes(app: FastifyInstance) {
-  app.post("/", async (req, res) => {
-    const createMealBodySchema = z.object({
+  app.post("/", { preHandler: [checkSessionIdExists] }, async (req, res) => {
+    const createMealBody = z.object({
       name: z.string(),
       description: z.string(),
       isOnDiet: z.boolean(),
       date: z.coerce.date(),
     });
 
-    const { name, description, isOnDiet, date } = createMealBodySchema.parse(
+    const { name, description, isOnDiet, date } = createMealBody.parse(
       req.body,
     );
 
@@ -27,4 +28,38 @@ export async function mealsRoutes(app: FastifyInstance) {
 
     return res.status(201).send("Refeição criada com sucesso!");
   });
+
+  app.get("/", { preHandler: [checkSessionIdExists] }, async (req, res) => {
+    const meals = await knex("meals")
+      .where({ user_id: req.user?.id })
+      .orderBy("date", "desc");
+
+    if (meals.length === 0) {
+      return res.status(404).send({ error: "Nenhuma refeição encontrada!" });
+    }
+
+    return res.status(200).send({
+      meals,
+    });
+  });
+
+  app.get(
+    "/:mealId",
+    { preHandler: [checkSessionIdExists] },
+    async (req, res) => {
+      const getMealParams = z.object({
+        mealId: z.string().uuid(),
+      });
+
+      const { mealId } = getMealParams.parse(req.params);
+
+      const meal = await knex("meals").where({ id: mealId }).first();
+
+      if (!meal) {
+        return res.status(404).send({ error: "Refeição não encontrada!" });
+      }
+
+      return res.status(200).send({ meal });
+    },
+  );
 }
